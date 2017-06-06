0861efe4cd9b0735b035733dfecbb7b68c004172
abhishek_patil <abhishek_patil@.persistent.co.in>
2017-06-06 15:20:22 +0530

    First Commit

---
+++ Helloworld/src/main/java/com/programcreek/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.programcreek.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	String message = "Welcome to Spring MVC!";
+ 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage() {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+	
+		return mv;
+	}
+	
+	
+}
